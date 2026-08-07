6cd4c31bb9f1e2f4cf661a8787c4a69b033ed7a8
juandiegoguzmanpinto-cell <juandiegoguzmanpinto@gmail.com>
2026-08-07 13:32:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const fechaEvento = new Date("August 29, 2026 18:00:00").getTime();
 
+setInterval(() => {
+
+    const ahora = new Date().getTime();
+
+    const diferencia = fechaEvento - ahora;
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+
+    document.getElementById("countdown").innerHTML =
+    dias + " días";
+
+},1000);
